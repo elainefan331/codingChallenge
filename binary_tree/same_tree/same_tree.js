62e40188c903233e03d0e4cf9c848499a303fae8
elaine <elainefan331@gmail.com>
2024-06-24 17:41:17 -0400

    add recursive solution to same_tress.js

--- binary_tree/same_tree/same_tree.js
+++ binary_tree/same_tree/same_tree.js
@@ -6,38 +6,53 @@ compare the value of two nodes
     - if couldn't find different between two trees --> return true
 */
 
+
+// iterative solution
+
+// const isSameTree = (p, q) => {
+//     if (p === null && q === null) return true;
+//     if ((p === null && q !== null) || (p !== null && q === null)) return false
+
+//     let stack1 = [p];
+//     let stack2 = [q];
+
+//     while(stack1.length > 0 || stack2.length > 0) {
+//         let curr1 = stack1.pop();
+//         let curr2 = stack2.pop();
+        
+//         if (curr1.val !== curr2.val) return false
+      
+//         if(curr1.left !== null && curr2.left !== null) {
+//             stack1.push(curr1.left);
+//             stack2.push(curr2.left);
+//         } else if (curr1.left === null && curr2.left !== null) {
+//             return false
+//         } else if (curr1.left !== null && curr2.left === null) {
+//             return false
+//         }
+
+//         if(curr1.right !== null && curr2.right !== null) {
+//             stack1.push(curr1.right);
+//             stack2.push(curr2.right);
+//         } else if (curr1.right === null && curr2.right !== null) {
+//             return false
+//         } else if (curr1.right !== null && curr2.right === null) {
+//             return false
+//         }
+        
+//     }
+
+//     return true;
+// }
+
+// recusive solution
 const isSameTree = (p, q) => {
     if (p === null && q === null) return true;
-    if ((p === null && q !== null) || (p !== null && q === null)) return false
+    if ((p === null && q !== null) || (p !== null && q === null)) return false;
+    if (p.val !== q.val) return false
 
-    let stack1 = [p];
-    let stack2 = [q];
+    let left = isSameTree(p.left, q.left);
+    let right = isSameTree(p.right, q.right);
 
-    while(stack1.length > 0 || stack2.length > 0) {
-        let curr1 = stack1.pop();
-        let curr2 = stack2.pop();
-        
-        if (curr1.val !== curr2.val) return false
-      
-        if(curr1.left !== null && curr2.left !== null) {
-            stack1.push(curr1.left);
-            stack2.push(curr2.left);
-        } else if (curr1.left === null && curr2.left !== null) {
-            return false
-        } else if (curr1.left !== null && curr2.left === null) {
-            return false
-        }
-
-        if(curr1.right !== null && curr2.right !== null) {
-            stack1.push(curr1.right);
-            stack2.push(curr2.right);
-        } else if (curr1.right === null && curr2.right !== null) {
-            return false
-        } else if (curr1.right !== null && curr2.right === null) {
-            return false
-        }
-        
-    }
-
-    return true;
+    return left && right;
 }
