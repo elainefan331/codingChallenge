6807bdaa70ccefc7744123d73a8e9734f799ab3a
elaine <elainefan331@gmail.com>
2024-08-07 08:24:25 +0800

    longest zigzag path.js completed

--- binary_tree_DFS/longest_zigzag_path/longest_zigzag_path.js
+++ binary_tree_DFS/longest_zigzag_path/longest_zigzag_path.js
@@ -7,3 +7,26 @@ helper function to calculate longest path
     - declare goLeft = true
     - if node.left === null, goLeft = false
 */
+
+const longestZigZag = (root) => {
+    let longestPath = 0
+    
+    const dfs = (node, goLeft, steps) => {
+        if (node) {
+            longestPath = Math.max(steps, longestPath)
+
+            if (goLeft) {
+                dfs(node.left, false, steps + 1)
+                dfs(node.right, true, 1)
+            } else {
+                dfs(node.right, true, steps + 1)
+                dfs(node.left, false, 1)
+            }
+        }
+    }
+
+    dfs(root, true, 0)
+    dfs(root, false, 0)
+    
+    return longestPath
+}
