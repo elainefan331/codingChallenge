28845c05edf9602681b20e9c6e4fc8b26dc2a5fb
elaine <elainefan331@gmail.com>
2024-08-24 16:06:31 -0400

    maximum level sum of binary tree.js

--- binary_tree_BFS/maximum_level_sum_of_binary_tree.js
+++ binary_tree_BFS/maximum_level_sum_of_binary_tree.js
@@ -2,6 +2,5 @@
 Create an integer variable maxSum to keep track of the maximum sum of node values at any level. We start with a large negative value.
 Create another variable ans to store the answer to the problem.
 Create another integer variable level to store the current level through which we are iterating. We initialize it with 0.
-Initialize a queue q of TreeNode and push root into it.
-
+Initialize a queue q of TreeNode and push root into i
 */
