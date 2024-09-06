43cb5f36ec7de1963ca5efa6f36f764f86a7e0d9
elainefan331 <elainefan331@gmail.com>
2024-09-06 12:47:37 -0400

    update number of islands.py

--- graphs/BFS/number_of_islands/number_of_islands.py
+++ graphs/BFS/number_of_islands/number_of_islands.py
@@ -1,15 +1,15 @@
+import collections
 # https://www.youtube.com/watch?v=pV2kpPD66nE
-# 
-# def isInBound(r, c):
-#   rowMax = len(grid)
-#   colMax = len(grid[0])
-#   return r in range(rowMax) and c in range(colMax):
-# 
-# 
+
+# rowMax = len(grid)
+# colMax = len(grid[0])
 # count = 0
-#
-# def BFS:
-#   q = the start point to explore
+# visited = set()
+
+# def BFS(r, c):
+#   q = collections.deque()
+#   q.append((r,c))
+#   visited.add((r,c))
 #   while q:
 #       r, c = q.popleft()
 #       visited.add((r,c))
@@ -17,8 +17,31 @@
 #           if isInBound(r, c) and (r, c) not in visited and grid[r][c] == "1":  ## check if r and c is in bound
 #           add (r, c) into q
 #   count + 1
+
 # return count
 
 
-def numIslands(grid):
+# def numIslands(grid):
+#     if not grid:
+#         return 0
     
+#     rowMax, colMax = len(grid), len(grid[0])
+#     visited = set()
+#     count = 0
+    
+    
+    
+    
+    
+#     return count
+
+
+q = collections.deque()
+q.append((0, 1))
+print("q", q)
+q.append((0, 2))
+print("q", q)
+c = q.popleft()
+print("pop out", c)
+# print("pop out", r)
+print('q', q)
