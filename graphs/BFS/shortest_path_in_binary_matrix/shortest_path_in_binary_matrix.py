a2f31f1fdeb8562e87c556eceaa3fd1b20c702ea
elainefan331 <elainefan331@gmail.com>
2024-09-09 14:45:20 -0400

    shortest path in binary matrix.py completed

--- graphs/BFS/shortest_path_in_binary_matrix/shortest_path_in_binary_matrix.py
+++ graphs/BFS/shortest_path_in_binary_matrix/shortest_path_in_binary_matrix.py
@@ -9,17 +9,19 @@ def shortestPathBinaryMatrix(grid):
     visited = set()
     q = collections.deque()
     q.append((0, 0, 1 ))
-    visited.add((0, 0, 1))
+    visited.add((0, 0))
     
     while q:
         row, col, layer = q.popleft()
+        if row == rowMax - 1 and col == colMax - 1:
+            return layer
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [-1, 1], [1, -1], [-1, -1]]
         for dir in directions:
             r = row + dir[0]
             c = col + dir[1]
-            if r == rowMax - 1 and c == colMax - 1:
-                return layer + 1
-            if r in range(rowMax) and c in range(colMax) and grid[r][c] == 0 and (r, c, layer + 1) not in visited:
+            if r in range(rowMax) and c in range(colMax) and grid[r][c] == 0 and (r, c) not in visited:
                 q.append((r, c, layer + 1))
-                visited.add((r, c, layer + 1))
+                visited.add((r, c))
+                
+    return -1
     
